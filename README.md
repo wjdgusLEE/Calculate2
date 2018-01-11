d96d2f338120d1777e5054e17ca4e27f88fd1545
wjdgusLEE <kjjlss@naver.com>
2018-01-11 13:30:54 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Calculate2
---
+++ src/Calc.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Calc {
+	
+	public static void main(String[] args) {
+		int casenum;
+		int num1, num2;
+		
+		Calc cal = new Calc();
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("[ ID : 1512173]");
+		System.out.println("[ Name : ÀÌÁ¤Çö]");
+		
+		System.out.println();
+		System.out.println("1. Add two numbers");
+		System.out.println("2. Subtract two numbers");
+		System.out.println("3. Multiply two numbers");
+		System.out.println("4. Quit");
+		
+		System.out.println();
+		System.out.print(">");
+		
+	}
+
+}
